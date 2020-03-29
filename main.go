0cee6b6fff0d9ab58918c2d6c8f36ef96afea5c6
Adam Inoue <adam@inoue.io>
2020-03-28 21:36:17 -0500

    prep add function for creation

--- main.go
+++ main.go
@@ -24,8 +24,8 @@ func main() {
 		}
 
 	// adding and removing entire budget items
-	case "add":
-		// add new budget item
+	case "add": // add new budget item
+		
 	case "delete":
 		// delete budget item
 
